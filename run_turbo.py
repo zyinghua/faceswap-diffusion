828ee0d41a01483ba9e41fe858be9a4a95b87723
zyinghua <49605250+zyinghua@users.noreply.github.com>
2025-11-24 00:23:25 -0500

    refactor

--- run_turbo.py
+++ run_turbo.py
@@ -1,17 +1,21 @@
-from diffusers import AutoPipelineForText2Image
+from pipelines.pipeline_stable_diffusion import StableDiffusionPipeline
+from pipelines.pipeline_stable_diffusion_xl import StableDiffusionXLPipeline
 import torch
 
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+idx = 1
+model = ["stabilityai/sd-turbo", "stabilityai/sdxl-turbo"][idx]
+pipeline = [StableDiffusionPipeline, StableDiffusionXLPipeline][idx]
 
-pipe = AutoPipelineForText2Image.from_pretrained(
-    "stabilityai/sd-turbo", 
+pipe = pipeline.from_pretrained(
+    model, 
     torch_dtype=torch.float16, 
     variant="fp16"
 ).to(device)
 
 my_prompts = [
-    "full head realistic shot of a person"
-] * 5
+    "full head portrait of a person"
+] * 1
 
 images = pipe(
     prompt=my_prompts, 
@@ -20,4 +24,4 @@ images = pipe(
 ).images
 
 for i, img in enumerate(images):
-    img.save(f"batch_result_{i}.png")
+    img.save(f"generated_image_{i}.png")
