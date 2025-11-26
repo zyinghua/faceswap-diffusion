98caeca704ae3902b56491f12bbfb32afcc28522
zyinghua <49605250+zyinghua@users.noreply.github.com>
2025-11-26 01:57:50 -0500

    change default effective bs to 4

--- train_controlnet.sh
+++ train_controlnet.sh
@@ -8,11 +8,11 @@ accelerate launch train_controlnet.py \
     --num_train_epochs 10 \
     --resolution 512 \
     --learning_rate 1e-4 \
-    --train_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --train_batch_size 2 \
+    --gradient_accumulation_steps 2 \
     --checkpointing_steps 1000 \
     --checkpoints_total_limit 5 \
-    --validation_steps 500 \
+    --validation_steps 1000 \
     --validation_prompt "high-quality professional photo of a face" "high-quality professional photo of a face" "high-quality professional photo of a face" \
     --validation_image "ffhq-dataset512-canny/canny/Part2/17090.png" "ffhq-dataset512-canny/canny/Part7/65173.png" "ffhq-dataset512-canny/canny/Part5/41191.png" \
     --num_validation_images 1 \
