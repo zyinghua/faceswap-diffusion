a384930574ea2b03cdecf4dc147c69a7ab0f91df
zyinghua <49605250+zyinghua@users.noreply.github.com>
2025-11-27 23:51:27 -0500

    revert val during training, default not use fixed timestep, ckpt steps * 10

--- train_controlnet.sh
+++ train_controlnet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#export HF_HUB_DISABLE_XET=1
+
 accelerate launch train_controlnet.py \
     --pretrained_model_name_or_path "stabilityai/sd-turbo" \
     --conditioning_channels 3 \
@@ -10,10 +12,9 @@ accelerate launch train_controlnet.py \
     --learning_rate 1e-5 \
     --train_batch_size 2 \
     --gradient_accumulation_steps 2 \
-    --checkpointing_steps 1000 \
+    --checkpointing_steps 10000 \
     --checkpoints_total_limit 5 \
-    --use_fixed_timestep \
-    # --validation_steps 1000 \
-    # --validation_prompt "high-quality professional photo of a face" "high-quality professional photo of a face" "high-quality professional photo of a face" \
-    # --validation_image "ffhq-dataset512-canny/canny/Part2/17090.png" "ffhq-dataset512-canny/canny/Part7/65173.png" "ffhq-dataset512-canny/canny/Part5/41191.png" \
-    # --num_validation_images 1 \
+    --validation_steps 500 \
+    --validation_prompt "high-quality professional photo of a face" "high-quality professional photo of a face" \
+    --validation_image "ffhq-dataset512-canny/canny/Part2/17090.png" "ffhq-dataset512-canny/canny/Part7/65173.png" \
+    # --use_fixed_timestep \
