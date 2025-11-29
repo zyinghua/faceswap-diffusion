3af2f1182a22b55297c69cd47ae83756621ebe7a
zyinghua <49605250+zyinghua@users.noreply.github.com>
2025-11-29 16:12:40 -0500

    update training settings, wrt 4 gpu training

--- train_controlnet.sh
+++ train_controlnet.sh
@@ -3,16 +3,17 @@
 #export HF_HUB_DISABLE_XET=1
 
 accelerate launch train_controlnet.py \
-    --pretrained_model_name_or_path "stabilityai/sd-turbo" \
+    --pretrained_model_name_or_path "Manojb/stable-diffusion-2-1-base" \
     --conditioning_channels 3 \
     --train_data_dir "ffhq-dataset512-canny" \
     --output_dir "controlnet-model" \
-    --num_train_epochs 10 \
+    --num_train_epochs 1 \
+    --max_train_steps 25000 \
     --resolution 512 \
     --learning_rate 1e-5 \
-    --train_batch_size 2 \
-    --gradient_accumulation_steps 2 \
-    --checkpointing_steps 10000 \
+    --train_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --checkpointing_steps 5000 \
     --checkpoints_total_limit 5 \
     --validation_steps 500 \
     --validation_prompt "high-quality professional photo of a face" "high-quality professional photo of a face" \
