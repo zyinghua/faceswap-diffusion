57c6729457cd17cb913df95e422fba2ac6f15fe3
zyinghua <49605250+zyinghua@users.noreply.github.com>
2025-12-03 16:33:48 -0500

    update controlnet inf to accept multiple generations

--- scripts/inference/run_controlnet_inference.py
+++ scripts/inference/run_controlnet_inference.py
@@ -12,15 +12,15 @@ import json
 
 BASE_MODEL = "Manojb/stable-diffusion-2-1-base"
 CONTROLNET_PATH = "" # pretrained controlnet path, ends with /controlnet
-PROMPT_JSONL_PATH = ""  # Path to JSONL file containing prompts (each line should have "text")
+METADATA_JSONL_PATH = ""  # Path to JSONL file containing prompts (each line should have "text")
 CONTROL_IMAGE = "" # canny image
 NUM_INFERENCE_STEPS = 20
-OUTPUT_PATH = "./output.png"
+OUTPUT_PATH = "./" # path to output dir
 SEED = random.randint(0, 1000000)
 DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 DTYPE = torch.bfloat16
 NEGATIVE_PROMPT = "noisy, blurry, low contrast, realism, photorealistic, vibrant, colorful, watermark, painting, drawing, illustration, glitch, deformed, mutated, cross-eyed, ugly, disfigured"
-
+SAMPLE_NUM = 10
 
 # Guidance parameter (default: 7.5):
 GUIDANCE_SCALE = 7.5
@@ -33,8 +33,8 @@ def main():
     control_image_filename = CONTROL_IMAGE.split("/")[-1]
     
     prompt = None
-    if PROMPT_JSONL_PATH:
-        with open(PROMPT_JSONL_PATH, 'r', encoding='utf-8') as f:
+    if METADATA_JSONL_PATH:
+        with open(METADATA_JSONL_PATH, 'r', encoding='utf-8') as f:
             for line in f:
                 line = line.strip()
                 if not line:
@@ -46,7 +46,7 @@ def main():
                     break
     
     if not prompt:
-        raise ValueError(f"Could not find prompt for {control_image_filename} in {PROMPT_JSONL_PATH}")
+        raise ValueError(f"Could not find prompt for {control_image_filename} in {METADATA_JSONL_PATH}")
     
     print(f"Loading ControlNet from: {CONTROLNET_PATH}")
     print(f"Prompt: {prompt}")
@@ -65,20 +65,24 @@ def main():
     pipe.enable_model_cpu_offload()
 
     control_image = load_image(CONTROL_IMAGE)
+
+    prompts = [prompt] * SAMPLE_NUM
+    negative_prompts = [NEGATIVE_PROMPT] * SAMPLE_NUM
     
     # Generate image
     generator = torch.manual_seed(SEED)
-    image = pipe(
-        prompt,
-        negative_prompt=NEGATIVE_PROMPT,
+    images = pipe(
+        prompts,
+        negative_prompt=negative_prompts,
         num_inference_steps=NUM_INFERENCE_STEPS, 
         generator=generator, 
         image=control_image,
         guidance_scale=GUIDANCE_SCALE,
         controlnet_conditioning_scale=CONTROLNET_CONDITIONING_SCALE
-    ).images[0]
+    ).images
     
-    image.save(OUTPUT_PATH)
+    for i, image in enumerate(images):
+        image.save(OUTPUT_PATH + f"generated_image{i}.png")
 
 
 if __name__ == "__main__":
